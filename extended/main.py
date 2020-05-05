f26b23b420702ff96eb301da9d3c39f4a782f065
Nicholas S Georgescu <nsgeorgescu@gmail.com>
2020-05-05 11:23:27 -0400

    fixed imports in main.py

--- extended/main.py
+++ extended/main.py
@@ -4,6 +4,7 @@ from .indexing import first, last, rotate_left, rotate_right, partition
 from .numbertests import even_q, odd_q
 from .nesting import nest_while_list, nest_while, nest, nest_list
 from .classes import up_to, sow_reap
+from .comprehensions import printr, check
 
 
 #%%
