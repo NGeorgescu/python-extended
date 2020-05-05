bc44a897c6ee1da944c8e9d6091a6e2b701efbe1
Nicholas S Georgescu <nsgeorgescu@gmail.com>
2020-05-05 11:02:12 -0400

    sow_reap

--- extended/main.py
+++ extended/main.py
@@ -3,7 +3,7 @@ from .iterable import tally, counts, counts_by, group_by, gather_by
 from .indexing import first, last, rotate_left, rotate_right, partition
 from .numbertests import even_q, odd_q
 from .nesting import nest_while_list, nest_while, nest, nest_list
-from .classes import up_to, throw_catch
+from .classes import up_to, sow_reap
 
 
 #%%
